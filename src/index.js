02a7b1731fd82e00d33683055654884f816f8ebe
Sonu Kumar <sonu.patna0808@gmail.com>
2021-08-11 10:49:38 +0530

    Improving CSS imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./assets/vendor/nucleo/css/nucleo.css";
 import "./assets/vendor/font-awesome/css/font-awesome.min.css";
 import "./assets/css/argon-design-system-react.css";
 
